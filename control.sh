1d502472c58806bb70676e5c6488ce24ff236aba
Yanlongli <ahlyl94@gmail.com>
2023-10-26 17:21:18 +0800

    control.sh add ghproxy

--- control.sh
+++ control.sh
@@ -8,9 +8,9 @@ chown $(id -u):$(id -g) $HOME/.kube/config
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+kubectl create -f https://ghproxy.com/https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
 
-curl -o custom-resources.yaml https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml
+curl -o custom-resources.yaml https://ghproxy.com/https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml
 
 sed -i 's@192.168.0.0/16@10.244.0.0/16@' custom-resources.yaml
 
